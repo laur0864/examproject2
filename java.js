920f2359e6edf07eb4372058eafb94402838db7e
Lasse843k <47578972+Lasse843k@users.noreply.github.com>
2019-05-23 15:43:47 +0200

    Fixed overlay så det virker som link

--- java.js
+++ java.js
@@ -13,24 +13,6 @@ function capture(){
     var video = document.getElementById('video');
 }
 
-
-/*    Accordion     */
-
-let acc = document.getElementsByClassName("accordion"); 
-let i;
-
-for (i = 0; i < acc.length; i++) {  
-  acc[i].addEventListener("click", function() { 
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling; 
-    if (panel.style.display === "block") { 
-      panel.style.display = "none";
-    } else {  
-      panel.style.display = "block"; 
-    }
-  });
-}
-    
     
     /* Nyt afsnit */
 =======
