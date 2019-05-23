0b224515b6586fd2d8294cb0637b0d03796a395a
Lasse843k <47578972+Lasse843k@users.noreply.github.com>
2019-05-23 14:12:08 +0200

    Java til java.js

--- java.js
+++ java.js
@@ -11,3 +11,21 @@ $('.video').parent().click(function () {
 function capture(){
     var video = document.getElementById('video');
 }
+
+
+/*    Accordion     */
+
+let acc = document.getElementsByClassName("accordion"); 
+let i;
+
+for (i = 0; i < acc.length; i++) {  
+  acc[i].addEventListener("click", function() { 
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling; 
+    if (panel.style.display === "block") { 
+      panel.style.display = "none";
+    } else {  
+      panel.style.display = "block"; 
+    }
+  });
+}
