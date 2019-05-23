0a9ddbfa6a0bf6c2cf96411fe15da54c6f9e9d33
Laurids Nørgaard <47400771+laur0864@users.noreply.github.com>
2019-05-23 14:11:36 +0200

    Footer fixet på alle sider + mindre rettelser

--- java.js
+++ java.js
@@ -1,10 +1,8 @@
 function myFunction() {
     var x, text;
   
-    // Get the value of the input field with id="numb"
     x = document.getElementById("numb").value;
   
-    // If x is Not a Number or less than one or greater than 10
     if (isNaN(x) || x < 1 || x > 48) {
       text = "Vi har desværre kun 48 soverpladser";
     } else {
